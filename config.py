914c61cd872153e359ea5d6b7ab5ef51c49a31c3
Purshotam Kumar <proustbritish38@gamil.com>
2026-05-12 20:48:37 +0000

    eda, and lstm part done

--- config.py
+++ config.py
@@ -1,26 +1,25 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).parent
-DATA_DIR = BASE_DIR / "data"
-PLOTS_DIR = BASE_DIR / "outputs" / "plots"
+base_dir = Path(__file__).parent
+data_dir = base_dir / "data"
+plots_dir = base_dir / "outputs" / "plots"
 
-TICKERS = {
-    "Banking":  "HDFCBANK.NS",
-    "IT":       "INFY.NS",
-    "Pharma":   "SUNPHARMA.NS",
-    "Auto":     "TATAMOTORS.NS",
-    "FMCG":     "HINDUNILVR.NS"
+tickers = {
+    "Banking": "HDFCBANK.NS",
+    "IT": "INFY.NS",
+    "Pharma": "SUNPHARMA.NS",
+    "FMCG": "HINDUNILVR.NS"
 }
 
-START_DATE   = "2021-01-01"
-END_DATE     = "2025-12-31"
-INTERVAL     = "1d"
+start_date = "2021-01-01"
+end_date = "2025-12-31"
+interval = "1d"
 
-TRAIN_END    = "2025-06-30"
-TEST_START   = "2025-07-01"
-TEST_END     = "2025-12-31"
+train_end = "2025-06-30"
+test_start = "2025-07-01"
+test_end = "2025-12-31"
 
-FORECAST_HORIZON  = 2
-VIRTUAL_CAPITAL   = 1_000_000
-LSTM_WINDOW       = 30
-RANDOM_SEED       = 42
+forecast_horizon  = 2
+virtual_capital   = 1_000_000
+lstm_window       = 30
+random_seed       = 42
